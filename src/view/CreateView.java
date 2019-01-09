e3bee08cd66f61199959dce0795fbdb19b45f7f8
Nick Coreus <nickster1283@hotmail.com>
2019-01-09 14:47:12 -0500

    updated

--- src/view/CreateView.java
+++ src/view/CreateView.java
@@ -4,16 +4,34 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.awt.Button;
 
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
+
 import org.jdatepicker.JDatePicker;
 
 import controller.ViewManager;
 
 @SuppressWarnings("serial")
 public class CreateView extends JPanel implements ActionListener {
+	
+	private JButton creatorButton;
+	private JButton cancelButton;
+	private JTextField firstname;
+	private JTextField lastname;
+	private JTextField datePicker;
+	private JTextField phone;
+	private JTextField address;
+	private JTextField city;
+	private JComboBox state;
+	private JTextField postal_code;
+	private JPasswordField pinField;
 	
 	private ViewManager manager;		// manages interactions between the views, model, and database
 	
@@ -38,23 +56,14 @@ public class CreateView extends JPanel implements ActionListener {
 	
 	private void initialize() {
 		
-		// TODO
-		//
-		// this is a placeholder for this view and should be removed once you start
-		// building the CreateView.
+		initButtons();
+		initfirstname();
+		initlastname();
+		//initdatePicker();
+		initphone();
 		
 		this.add(new javax.swing.JLabel("New Account", javax.swing.SwingConstants.CENTER));
 		
-		JTextField firstname = new JTextField();
-		firstname.addActionListener(this);
-		firstname.setVisible(true);
-		
-		JTextField lastname = new JTextField();
-		lastname.addActionListener(this);
-		
-		JDatePicker datePicker = new JDatePicker();
-		 
-		CreateView.add(datePicker);
 
 		//
 		// this is where you should build the CreateView (i.e., all the components that
@@ -64,7 +73,48 @@ public class CreateView extends JPanel implements ActionListener {
 		// positioning your components.
 	}
 	
-
+	private void initfirstname() {
+		JLabel label = new JLabel("First Name", SwingConstants.LEFT);
+		
+		JTextField initfirstname = new JTextField(30);
+		initfirstname.addActionListener(this);
+		initfirstname.setVisible(true);
+		
+		this.add(label);
+		this.add(initfirstname);
+	}
+	
+	private void initlastname() {
+		JLabel label = new JLabel("Last Name", SwingConstants.LEFT);
+		JTextField initlastname = new JTextField(30);
+		initlastname.addActionListener(this);
+		initlastname.setBounds(100,45,100,45);
+		this.add(initlastname);
+		initlastname.setVisible(true);
+	}
+	
+	/*private void initdatePicker() {
+		JDatePicker initdatePicker = new JDatePicker();
+		CreateView.add(initdatePicker);
+	}*/
+	private void initphone() {
+		JLabel label = new JLabel("Phone", SwingConstants.LEFT);
+		JTextField initphone = new JTextField(10);
+		initphone.addActionListener(this);
+		initphone.setVisible(true);
+	}
+	
+	private void initButtons() {
+		// TODO Auto-generated method stub
+		Button creatorButton = new Button("Create Account:");
+		creatorButton.addActionListener(this);
+		add(creatorButton);
+        
+        Button cancelButton = new Button("Cancel Account:");
+        cancelButton.addActionListener(this);
+        add(cancelButton);
+	}
+	
 	/*
 	 * CreateView is not designed to be serialized, and attempts to serialize will throw an IOException.
 	 * 
@@ -86,7 +136,14 @@ public class CreateView extends JPanel implements ActionListener {
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+		Object source = e.getSource();
+		if (source.equals(creatorButton)) {
+    	manager.switchTo(ATM.HOME_VIEW);
+		} 
+			else if (source.equals(cancelButton)) {
+		    	manager.switchTo(ATM.LOGIN_VIEW);
+				} else 
+					manager.switchTo(ATM.LOGIN_VIEW);
 		// TODO
 		//
 		// this is where you'll setup your action listener, which is responsible for
