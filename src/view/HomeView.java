5b2cf7e29c1afff6baec39063409395779183608
ncoreus1 <43218019+ncoreus1@users.noreply.github.com>
2019-01-22 22:21:24 -0500

    Update HomeView.java

--- src/view/HomeView.java
+++ src/view/HomeView.java
@@ -1,18 +1,31 @@
 package view;
 
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-
 import controller.ViewManager;
+import model.BankAccount;
+import model.User;
+import view.LoginView;
 
 @SuppressWarnings("serial")
 public class HomeView extends JPanel implements ActionListener {
 	
+	private JButton logoutButton;
+	private JButton withdrawButton;
+	private JButton depositButton;
+	private JButton transferButton;
+	private JButton informationButton;
+	private JButton closeButton;
 	private ViewManager manager;		// manages interactions between the views, model, and database
+	private JLabel accountNumberLabel;
+	private BankAccount account;
+	private User user;
 	
 	/**
 	 * Constructs an instance (or objects) of the HomeView class.
@@ -27,6 +40,62 @@ public class HomeView extends JPanel implements ActionListener {
 		initialize();
 	}
 	
+	private void initLogoutButton() {
+		logoutButton = new JButton("Logout");
+		logoutButton.setBounds(30, 30, 200, 35);
+		logoutButton.addActionListener(this);
+
+		this.add(logoutButton);
+	}
+	
+	private void initWithdrawButton() {
+		withdrawButton = new JButton("Withdraw");
+		withdrawButton.setBounds(30, 120, 200, 35);
+		withdrawButton.addActionListener(this);
+
+		this.add(withdrawButton);
+	}
+	
+	private void initDepositButton() {
+		depositButton = new JButton("Deposit");
+		depositButton.setBounds(30, 160, 200, 35);
+		depositButton.addActionListener(this);
+
+		this.add(depositButton);
+	}
+	
+	private void initCloseButton() {
+		closeButton = new JButton("Close");
+		closeButton.setBounds(30, 280, 200, 35);
+		closeButton.addActionListener(this);
+
+		this.add(closeButton);
+	}
+	
+	private void initTransferButton() {
+		transferButton = new JButton("Transfer");
+		transferButton.setBounds(30, 200, 200, 35);
+		transferButton.addActionListener(this);
+
+		this.add(transferButton);
+	}
+	
+	private void initInformationButton() {
+		informationButton = new JButton("Information");
+		informationButton.setBounds(30, 240, 200, 35);
+		informationButton.addActionListener(this);
+
+		this.add(informationButton);
+	}
+	
+	public void initAccountNumber() {
+		accountNumberLabel = new JLabel();
+		accountNumberLabel.setText("Hello " + account.getUser().getFirstName() + " " + account.getUser().getLastName() + ", your account number is " + account.getAccountNumber() + " and your balance is " + account.getFormattedBalance());
+		accountNumberLabel.setBounds(10, 75, 500, 35);
+		accountNumberLabel.setFont(new Font("DialogInput", Font.PLAIN, 8));
+		
+		this.add(accountNumberLabel);
+	}
 	///////////////////// PRIVATE METHODS /////////////////////////////////////////////
 	
 	/*
@@ -42,6 +111,8 @@ public class HomeView extends JPanel implements ActionListener {
 		
 		this.add(new javax.swing.JLabel("HomeView", javax.swing.SwingConstants.CENTER));
 		
+		this.setLayout(null);
+		
 		// TODO
 		//
 		// this is where you should build the HomeView (i.e., all the components that
@@ -49,6 +120,21 @@ public class HomeView extends JPanel implements ActionListener {
 		//
 		// feel free to use my layout in LoginView as an example for laying out and
 		// positioning your components.
+
+		initLogoutButton();
+		initWithdrawButton();
+		initDepositButton();
+		initTransferButton();
+		initInformationButton();
+		initCloseButton();
+	}
+	
+	public void setAccount(BankAccount account) {
+		this.account = account;
+		this.user = account.getUser();
+		this.removeAll();
+		this.initialize();
+		initAccountNumber();
 	}
 	
 	/*
@@ -72,7 +158,30 @@ public class HomeView extends JPanel implements ActionListener {
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+		Object source = e.getSource();
+		if (source.equals(logoutButton)) {
+			manager.logout();			
+		} 
+		if (source.equals(withdrawButton)) {
+			this.remove(accountNumberLabel);
+			manager.switchTo(ATM.WITHDRAW_VIEW);
+		} 
+		if (source.equals(depositButton)) {
+			this.remove(accountNumberLabel);
+			manager.switchTo(ATM.DEPOSIT_VIEW);
+		} 
+		if (source.equals(transferButton)) {
+			this.remove(accountNumberLabel);
+			manager.switchTo(ATM.TRANSFER_VIEW);
+		} 
+		if (source.equals(informationButton)) {
+			this.remove(accountNumberLabel);
+			manager.switchTo(ATM.INFORMATION_VIEW);
+			manager.setAccount(account);
+		} 
+		if (source.equals(closeButton)) {
+			manager.closeAccount();
+		} 
 		// TODO
 		//
 		// this is where you'll setup your action listener, which is responsible for
